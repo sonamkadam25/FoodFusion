a739f4a7c1ac1c606964e0f04fbf05ab2858d590
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2024-11-08 12:05:37 +0530

    Update MyOrder.js

--- frontend/src/SCREENS/MyOrder.js
+++ frontend/src/SCREENS/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5000/api/auth/myOrderData", {
+    await fetch("https://food-fusion-web.vercel.app/api/auth/myOrderData", {
       method: "POST", 
       headers: {
         "Content-Type": "application/json",
