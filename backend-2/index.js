1be36ad35277a4695649c6a4380d9dbab8cc4e87
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2024-11-08 11:33:58 +0530

    Update index.js

--- backend-2/index.js
+++ backend-2/index.js
@@ -8,7 +8,7 @@ mongoDB();
 
 app.use(
   cors({
-    origin: ["https://food-fusion-chi.vercel.app"],
+    origin: ["https://food-fusion-web.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
