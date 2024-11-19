16afabafa4099cfa05b4c64692173ce50aba2cec
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2024-11-20 00:56:57 +0530

    Update index.js

--- backend-2/index.js
+++ backend-2/index.js
@@ -1,39 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const mongoDB = require("./db");
-
-// const app = express();
-// const port = 5000;
-
-// mongoDB();
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-
-// app.use(express.json());
-
-// app.use("/api", require("./Routes/CreateUser"));
-// app.use("/api", require("./Routes/DisplayData"));
-// app.use("/api/auth", require("./Routes/OrderData"));
-
-// if (process.env.NODE_ENV !== "test") {
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//   });
-// }
-
-// module.exports = app;
-
-
-
-
-
-
 
 
 const express = require("express");
